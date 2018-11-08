23bf73e8370999133dcdf2abc94e4ad674114ceb
Adam Baker <adamb924@gmail.com>
2018-11-08 10:06:32 -0600

    More demo queries

--- demo-queries.sql
+++ demo-queries.sql
@@ -1,6 +1,5 @@
 -- These are just some interesting queries to run.
-.mode column
-.output demo-output.txt
+.mode list
 
 -- Get all of the unique parsing codes
 SELECT DISTINCT(parsing_code) FROM sblgnt ORDER BY parsing_code ASC;
@@ -22,3 +21,23 @@ SELECT lemma,tense,COUNT(tense) FROM sblgnt WHERE part_of_speech='verb' GROUP BY
 
 -- Same as above, but in John
 SELECT lemma,tense,COUNT(tense) FROM sblgnt WHERE part_of_speech='verb' AND book_name='John' GROUP BY lemma,tense ORDER BY lemma ASC;
+
+-- All combinations of tenses and moods
+SELECT DISTINCT tenses.human_readable AS tense,moods.human_readable AS mood FROM tenses,moods;
+
+-- How often the various tense/mood combinations appear in each lemma
+SELECT lemma,mood,tense,COUNT(*) AS ncount FROM sblgnt WHERE part_of_speech='verb' GROUP BY lemma,mood,tense ORDER BY lemma ASC;
+
+-- How often the various tense/mood combinations appear in each lemma, in John
+SELECT lemma,mood,tense,COUNT(*) AS ncount FROM sblgnt WHERE part_of_speech='verb' AND book_name='John' GROUP BY lemma,mood,tense ORDER BY lemma ASC;
+
+-- How often the various tense/mood combinations appear in each lemma, including zero counts
+SELECT combos.lemma,combos.mood,combos.tense,IFNULL(ncount,0) FROM
+	(SELECT DISTINCT tenses.human_readable AS tense,moods.human_readable AS mood,lemma FROM tenses,moods,sblgnt WHERE part_of_speech='verb') combos
+		LEFT JOIN
+	(SELECT lemma,mood,tense,COUNT(*) AS ncount FROM sblgnt WHERE part_of_speech='verb' GROUP BY lemma,mood,tense ORDER BY lemma ASC) counts
+	ON counts.lemma=combos.lemma AND counts.mood=combos.mood AND counts.tense=combos.tense
+	ORDER BY combos.lemma,combos.mood,combos.tense;
+	
+-- How often the various tense/mood combinations appear in each book
+SELECT book_name,mood,tense,COUNT(*) AS ncount FROM sblgnt WHERE part_of_speech='verb' GROUP BY book_name,mood,tense ORDER BY book_number ASC;
